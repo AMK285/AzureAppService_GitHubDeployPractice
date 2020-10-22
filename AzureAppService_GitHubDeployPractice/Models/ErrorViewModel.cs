cd104e08814646beb7d1b01ab09806bdece606de
Anup <anup.dhenry@gmail.com>
2020-10-21 23:44:30 -0700

    First commit

---
+++ AzureAppService_GitHubDeployPractice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureAppService_GitHubDeployPractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
